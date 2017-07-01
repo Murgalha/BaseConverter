c61723b6a634527c564ba3d2852865db84a118c9
Murgalha <murilobaldi1@gmail.com>
2017-07-01 20:47:44 -0300

    Add project files.

---
+++ BaseConverter.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BaseConverter {
+
+    static class BaseConverter {
+
+        public static bool IsBinary(string s) {
+
+            int i;
+
+            for (i = 0; i < s.Length; i++) {
+                if (s[i] != '1' && s[i] != '0') {
+                    return false;
+                }
+            }
+
+            if(s.Length > 64)
+                return false;
+            return true;
+        }
+
+        public static string Dec2Bin(string s) {
+
+            string result = "";
+            ulong n = Convert.ToUInt64(s);
+            List<String> remainders = new List<String>();
+            ulong q = 0, r;
+
+            while(n >= 2) {
+                q = n / 2;
+                r = n % 2;
+                remainders.Add(r.ToString());
+                n = q;
+            }
+
+            result = q.ToString();
+
+            remainders.Reverse();
+            List<String>.Enumerator it = remainders.GetEnumerator();
+
+            do {
+                result += it.Current;
+            } while(it.MoveNext());
+
+            return result;
+        }
+
+        public static string Dec2Hex(string s) {
+
+            string result = "";
+            s = s.ToUpper();
+            ulong n = Convert.ToUInt64(s);
+            ulong q = n%16, r;
+            List<ulong> remainders = new List<ulong>();
+
+            while (n >= 16) {
+                q = n / 16;
+                r = n % 16;
+                remainders.Add(r);
+                n = q;
+            }
+
+            remainders.Add(q);
+
+            remainders.Reverse();
+
+            List<ulong>.Enumerator it = remainders.GetEnumerator();
+
+            do {
+                if(it.Current == 10)
+                    result += 'A';
+                else if(it.Current == 11)
+                    result += 'B';
+                else if(it.Current == 12)
+                    result += 'C';
+                else if(it.Current == 13)
+                    result += 'D';
+                else if(it.Current == 14)
+                    result += 'E';
+                else if(it.Current == 15)
+                    result += 'F';
+                else
+                    result += it.Current.ToString();
+            } while(it.MoveNext());
+
+            return result;
+        }
+        /* dec2oct */
+
+        public static string Bin2Dec(string s) {
+
+            string result;
+            int i, k;
+            int p = 0;
+            ulong sum = 0;
+
+            for(i = s.Length-1; i >= 0; i--) {
+
+                if(s[i] == '1') {
+                    sum += (ulong)(Math.Pow(2, (double)p));
+                }
+                p++;
+            }
+
+            result = sum.ToString();
+
+            return result;
+        }
+
+        /* bin2hex */
+        /* bin2oct */
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BaseConverter;
+
+
+
+namespace BaseConverter {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            InitializeComboBox();
+            this.ResizeMode = System.Windows.ResizeMode.NoResize;
+        }
+
+        public void InitializeComboBox() {
+            ComboBox1.Items.Add("Decimal");
+            ComboBox1.Items.Add("Binary");
+            ComboBox1.Items.Add("Hexadecimal");
+            ComboBox1.Items.Add("Octal");
+            ComboBox2.Items.Add("Decimal");
+            ComboBox2.Items.Add("Binary");
+            ComboBox2.Items.Add("Hexadecimal");
+            ComboBox2.Items.Add("Octal");
+            ComboBox1.Text = "Decimal";
+            ComboBox2.Text = "Decimal";
+        }
+
+        private void SwapButton_Click(object sender, RoutedEventArgs e) {
+            Object aux = ComboBox1.SelectedItem;
+            ComboBox1.SelectedItem = ComboBox2.SelectedItem;
+            ComboBox2.SelectedItem = aux;
+        }
+
+        private void ConvertButton_Click(object sender, RoutedEventArgs e) {
+
+            if(ComboBox1.SelectedItem.ToString() == "Decimal") {
+                ulong n;
+                WarningText.Text = "";
+
+                try {
+                    n = Convert.ToUInt64(TextBox1.Text.ToString());
+                }
+                catch(FormatException) {
+                    WarningText.Text = "Error! Invalid number.";
+                    return;
+                }
+                catch(OverflowException) {
+                    WarningText.Text = "Error! Type a smaller number.";
+                    return;
+                }
+
+                if(ComboBox2.SelectedItem.ToString() == "Binary") {
+                    string result = "";
+
+                    result = BaseConverter.Dec2Bin(TextBox1.Text.ToString());
+
+                    TextBox2.Text = result;
+                }
+
+                else if(ComboBox2.SelectedItem.ToString() == "Hexadecimal") {
+                    string result;
+
+                    result = BaseConverter.Dec2Hex(TextBox1.Text.ToString());
+                    TextBox2.Text = result;
+                }
+
+                else if(ComboBox2.SelectedItem.ToString() == "Octal") {
+                    // Dec2Oct
+                }
+
+                else {
+                    TextBox2.Text = TextBox1.Text;
+                }
+            }
+
+            else if(ComboBox1.SelectedItem.ToString() == "Binary") {
+
+                if (!BaseConverter.IsBinary(TextBox1.Text.ToString())) {
+                    WarningText.Text = "Error! Invalid number.";
+                    return;
+                }
+
+                if(ComboBox2.SelectedItem.ToString() == "Decimal") {
+                    string result = "";
+
+                    result = BaseConverter.Bin2Dec(TextBox1.Text.ToString());
+
+                    TextBox2.Text = result;
+                }
+
+                else if(ComboBox2.SelectedItem.ToString() == "Hexadecimal") {
+
+                }
+
+                else if(ComboBox2.SelectedItem.ToString() == "Octal") {
+
+                }
+
+                else {
+                    TextBox2.Text = TextBox1.Text;
+                }
+            }
+
+            else if(ComboBox1.SelectedItem.ToString() == "Hexadecimal") {
+
+                if(ComboBox2.SelectedItem.ToString() == "Binary") {
+
+                }
+
+                else if(ComboBox2.SelectedItem.ToString() == "Decimal") {
+
+                }
+
+                else if(ComboBox2.SelectedItem.ToString() == "Octal") {
+
+                }
+
+                else {
+                    TextBox2.Text = TextBox1.Text;
+                }
+            }
+
+            else if(ComboBox1.SelectedItem.ToString() == "Octal") {
+
+                if(ComboBox2.SelectedItem.ToString() == "Binary") {
+
+                }
+
+                else if(ComboBox2.SelectedItem.ToString() == "Decimal") {
+
+                }
+
+                else if(ComboBox2.SelectedItem.ToString() == "Hexadecimal") {
+
+                }
+
+                else {
+                    TextBox2.Text = TextBox1.Text;
+                }
+            }
+        }
+    }
+}
